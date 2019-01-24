d62462f3b6420bacfcdfe7629472d7db8891bfcb
kevin <kevin1104cosner@hotmail.com>
2019-01-24 09:11:40 -0500

    Gitacora saved 1 changed files

--- ver.php
+++ ver.php
@@ -7,30 +7,38 @@ require_once "controlador/cargar.php";
 ?>
 <!DOCTYPE html>
 <html>
+<?php
+require_once "modulo/conexion.php";
+require_once "modulo/base.php";
+require_once "controlador/cargar.php";
+
+?>
+
+<!DOCTYPE html>
+<html >
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ver Productos</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ver Producto</title>
 </head>
 <body>
     <h1>LISTA DE PRODUCTOS</h1>
-    <h1>KEVIN MALDONADO</h1>
+    <h2>Joel Castro</h2>
     <div>
         <form method="get">
-        <input type ="text" name="buscar"></td>
-        <input type ="submit" value ="Buscar"></td>
+            <input type='text' name='buscar'>
+            <input type='submit' value='Buscar'>
         </form>
-
     </div>
-     <?php
-     
-     if(isset($_GET['buscar'])){
-         buscar($_GET['buscar']);
-     }else{
+    <?php 
+    if(isset($_GET['buscar'])){
+        buscar($_GET['buscar']);
+    }else{
         cargar();
-     }
-     ?>
-    <div><br><a href = "insertar.html">Regresar</a></div> 
-
+    }
+    ?>
 </body>
+        <div>
+            <a href="insertar.html">Regresar</a>
+        </div>
 </html>
