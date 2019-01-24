e2f314e712108b312cf5fabdf161c8236026e68a
kevin <kevin1104cosner@hotmail.com>
2019-01-23 20:18:18 -0500

    Gitacora saved 1 changed files

--- ver.php
+++ ver.php
@@ -9,11 +9,12 @@ require_once "controlador/cargar.php";
 <html >
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content=":id=id">
     <title>Ver Producto</title>
 </head>
 <body>
     <h1>LISTA DE PRODUCTOS</h1>
     <?php cargar(); ?>
+
 </body>
 </html>
