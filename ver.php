c20157d30e9f2879eb6f5e6217c224f4a7d132d1
kevin <kevin1104cosner@hotmail.com>
2019-01-24 09:06:50 -0500

    Gitacora saved 1 changed files

--- ver.php
+++ ver.php
@@ -14,7 +14,7 @@ require_once "controlador/cargar.php";
 </head>
 <body>
     <h1>LISTA DE PRODUCTOS</h1>
-    <h1>KEVIN MALDONADO</h1>
+    <h1>DAVID PILLAJO</h1>
     <div>
         <form method="get">
         <input type ="text" name="buscar"></td>
