0a4c38122ffd4fec83cc8a9eec242533eaad694e
kevin <kevin1104cosner@hotmail.com>
2019-01-24 07:44:42 -0500

    Gitacora saved 1 changed files

--- ver.php
+++ ver.php
@@ -2,6 +2,7 @@
 require_once "modulo/conexion.php";
 require_once "modulo/base.php";
 require_once "controlador/cargar.php";
+require_once "controlador/buscar.php";
 
 ?>
 
@@ -15,7 +16,18 @@ require_once "controlador/cargar.php";
 <body>
     <h1>LISTA DE PRODUCTOS</h1>
     <?php cargar(); ?>
-    <div><br><a href ="insertar.html">Regresar</a></div>
+</body>
+</html>
 
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>buscar Producto</title>
+</head>
+<body>
+    <h1>LISTA DE PRODUCTOS</h1>
+    <?php buscar(); ?>
 </body>
 </html>
