9c3ab3d706d0c78d791667ed451ed485bda8d642
kevin <kevin1104cosner@hotmail.com>
2019-01-24 08:48:48 -0500

    Gitacora saved 1 changed files

--- controlador/buscar.php
+++ controlador/buscar.php
@@ -2,28 +2,6 @@
 require_once('../modulo/conexion.php');
 require_once('../modulo/base.php');
 
-function buscar($nombre){
-    $consulta = new base ();
-    $filas = $consulta->buscarProductos();
-    echo "<table border = 1 bordercolor=blue>
-        <th>ID</th>
-        <th>NOMBRE</th>
-        <th>DESCRIPCION</th>
-        <th>CATEGORIA</th>
-        <th>PRECIO</th>";
-        if (isset($filas)){
-            foreach($filas as $fila){
-                echo "<tr>";
-                echo "<td>".$fila['id']. "</td>";
-                echo "<td>".$fila['nombre']. "</td>";
-                echo "<td>".$fila['descripcion']. "</td>";
-                echo "<td>".$fila['categoria']. "</td>";
-                echo "<td>".$fila['precio']. "</td>";
-                echo "<td><a href = 'controlador/eliminar.php?id=".$fila['nombre']."'>Buscar</td>";
-                echo "</tr>" ;
-            }
-        }
-    echo "</table>";
 
 }
 ?>
